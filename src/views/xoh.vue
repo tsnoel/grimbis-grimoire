003bdc58828b23b78902c73cb7f09e7c217d3caa
Tim Noel <tsnoel0@gmail.com>
2021-07-17 03:42:58 +0100

    Add character information.

--- src/views/xoh.vue
+++ src/views/xoh.vue
@@ -2,7 +2,6 @@
     <el-row :gutter="20" class="character">
         <el-col :xs="24" :sm="8" :md="6" :xl="4">
             <el-card class="meta">
-                {{meta}}
             </el-card>
         </el-col>
         <el-col :xs="24" :sm="8" :md="6" :xl="4">
@@ -12,6 +11,13 @@
 
 <script>
 import { ElCol, ElRow, ElCard } from 'element-plus';
+
+import spells from '../assets/spells.json';
+
+import actions from '../assets/xoh/actions.json';
+import features from '../assets/xoh/features.json';
+import personas from '../assets/xoh/personas.json';
+import background from '../assets/xoh/background.json';
 
 const HD = {
     'Warlock': 8,
@@ -51,113 +57,27 @@ name: 'xoh',
     },
     data() {
         return {
-            meta: {
-                name: 'Papa Kap',
-                background: {
-                    name: ' Folk Hero',
-                    description: 'You come from a humble social rank, ' +
-                        'but you are destined for so much more. ' +
-                        'Already the people of your home village ' +
-                        'regard you as their champion, and your ' +
-                        'destiny calls you to stand against the ' +
-                        'tyrants and monsters that threaten the ' +
-                        'common folk everywhere.',
-                    skills: 'Animal Handling, Survival',
-                    tools: 'One type of artisan’s tools, ' +
-                        'vehicles (land)',
-                    equipment: 'A set of artisan’s tools (one of ' +
-                        'your choice), a shovel, an iron pot, a set ' +
-                        'of common clothes, and a pouch containing ' +
-                        '10 gp',
-                    feature: 'Rustic Hospitality'
-                },
-                player: 'Tim',
-                race: 'Dwarf',
-                alignment: 'Neutral Good',
-                exp: 'Milestone'
-            },
+            actions,
+            background,
+            features,
+            personas,
             level: {
                 'Monk': {level: 8, sub: 'Way of the Open Hand', color: COLORS.Monk},
                 'Warlock': {level: 2, sub: undefined, color: COLORS.Warlock}
             },
-            // inspiration: false,
-            // defenses: '',
-            // conditions: '',
-            // senses: {},
-            personas: {
-                'Dok': {
-                    title: 'The people\'s Bugbear',
-                    description: 'A bugbear with decidedly ' +
-                        'un-bugbear goals: to be good, to have ' +
-                        'integrity, to be balanced, to improve ' +
-                        'other\'s conditions. The most likely to ' +
-                        'start a union.'
-                },
-                'Papa Kap': {
-                    title: 'The Dwarven Chef',
-                    description: 'A grandfatherly dwarf man. ' +
-                        'Proficient with cook\'s tools. Shared ' +
-                        'among the monastery changelings to help ' +
-                        'with food preparation duties.'
-                },
-                'Xoh': {
-                    title: 'The Neurotic Changeling',
-                    description: 'A young woman who uses alternate ' +
-                        'personas to make up for her perceived ' +
-                        'shortcomings.',
-                    alignment: 'Neutral Good',
-                    gender: 'F',
-                    eyes: 'White',
-                    size: 'Medium',
-                    height: '5\'7"',
-                    faith: '--',
-                    hair: 'White',
-                    skin: 'Pale',
-                    age: '18',
-                    weight: '123lb.',
-                    traits: 'I judge people by their actions, ' +
-                        'not their words.',
-                    ideals: 'Fairness. No one should get ' +
-                        'preferential treatment before the law, ' +
-                        'and no one is above the law. (Lawful)',
-                    bonds: 'I protect those who cannot protect ' +
-                        'themselves.',
-                    flaws: 'I\'m convinced of the significance of ' +
-                        'my destiny, and blind to my shortcomings ' +
-                        'and the risk of failure.'
-                },
-                '"Xed" (Kevin)': {
-                    title: 'The Angsty Drow',
-                    description: 'A teenage boy. Lover of dark ' +
-                        'clothes, fantasy emo, and trying to look cool.'
-                },
-                'Gob': {
-                    title: 'The Investigative Hobgoblin',
-                    description: 'Much shorter than his counterparts,' +
-                        ' this hobgoblin is much more interested in ' +
-                        'deduction than violence.'
-                },
-                'Wrett': {
-                    title: 'The Beefy Lizardman',
-                    description: 'A mute, muscled, anthropomorphic ' +
-                        'bearded dragon. Used for pranks, tricks, ' +
-                        'and intimidation.'
-                },
-                'Zazz': {
-                    title: 'The Human Monk',
-                    description: 'A modest young man with shaved ' +
-                        'head. Athletic build. Loves Horses. ' +
-                        'Loves light magic.'
-                }
-            },
             proficiency: 4,
-            ac: 0,
-            hd: [],
-            initative: 0,
             hp: {
-                // current: '',
-                // temp: '',
-                max: 0
+                current: 49,
+                temp: 0,
+                max: 0 // beforeMount
+            },
+            spellSlots: {
+                current: 1,
+                max: 2
+            },
+            ki: {
+                current: 7,
+                max: 8
             },
             abilities: {
                 'STR': {val: 7, prof: true},
@@ -167,10 +87,11 @@ name: 'xoh',
                 'WIS': {val: 14, prof: false},
                 'CHA': {val: 14, prof: false}
             },
-            skills: {},
             skillProfs: ['Acrobatics', 'Animal Handling', 'Athletics',
                 'Insight', 'Persuasion', 'Survival'],
             speed: {'Walking': '45\''},
+            spells: ['Light', 'Sacred Flame', 'Minor Illusion', 'Eldritch Blast',
+                'Cure Wounds', 'Healing Elixir', 'Hex'],
             proficiencies: {
                 armor: 'Light Armor',
                 weapons: 'Shortsword, Simple Weapons',
@@ -178,74 +99,12 @@ name: 'xoh',
                     'Smith\'s Tools, Vehicles (Land)',
                 languages: 'Common, Dwarvish, Undercommon'
             },
-            features: {
-                'Rustic Hospitality': {
-                    source: {type: 'Background', value: 'Folk Hero'},
-                    description: 'Since you come from the ranks of the common ' +
-                        'folk, you fit in among them with ease. You can find a place to ' +
-                        'hide, rest, or recuperate among other commoners, unless you have ' +
-                        'shown yourself to be a danger to them. They will shield you from ' +
-                        'the law or anyone else searching for you, though they will not risk ' +
-                        'their lives for you.'
-                },
-                'Unarmed Defense': {
-                    source: {type: 'Class', value: 'Monk', level: 1},
-                    description: 'Beginning at 1st level, while you are wearing no armor and not wielding a shield, your AC equals 10 + your Dexterity modifier + your Wisdom modifier.'
-                },
-                'Martial Arts': {
-                    source: {type: 'Class', value: 'Monk', level: 1},
-                    description:' At 1st level, your practice of martial arts gives you mastery of combat styles that use unarmed strikes and monk weapons, which are shortswords and any simple melee weapons that don’t have the two-handed or heavy property. \n\nYou gain the following benefits while you are unarmed or wielding only monk weapons and you aren’t wearing armor or wielding a shield: \n\n- You can use Dexterity instead of Strength for the attack and damage rolls of your unarmed strikes and monk weapons.\n- You can roll a d4 in place of the normal damage of your unarmed strike or monk weapon. This die changes as you gain monk levels: 1d4 (1-4), 1d6 (5-10), 1d8 (11-16), and 1d10 (17-20).\n- When you use the Attack action with an unarmed strike or a monk weapon on your turn, you can make one unarmed strike as a bonus action. For example, if you take the Attack action and attack with a quarterstaff, you can also make an unarmed strike as a bonus action, assuming you haven’t already taken a bonus action this turn.'
-                },
-                "Ki": {
-                    "source": {"type": "Class", "value": "Monk", "level": 2},
-                    "description": ""
-                },
-                "Unarmored Movement": {
-                    "source": {"type": "Class", "value": "Monk", "level": 2},
-                    "description": "Starting at 2nd level, your speed increases by 10' while you are not wearing armor or wielding a shield. This bonus increases when you reach certain monk levels: +10' (2-5), +15' (6-9), +20' (10-13), +25' (14-17), +30' (18-20)."
-                },
-                'Open Hand Technique': {
-                    source: {type: 'Class', value: 'Monk', sub: 'Way of the Open Hand', level: 3},
-                    description: 'Starting when you choose this tradition at 3rd level, you can manipulate your enemy’s ki when you harness your own. Whenever you hit a creature with one of the attacks granted by your Flurry of Blows, you can impose one of the following effects on that target:\n\n- It must succeed on a Dexterity saving throw or be knocked prone.\n- It must make a Strength saving throw. If it fails, you can push it up to 15 feet away from you.\n- It can’t take reactions until the end of your next turn.'
-                },
-                'Deflect Missiles': {
-                    source: {type: 'Class', value: 'Monk', level: 3},
-                    description: 'Starting at 3rd level, you can use your reaction to deflect or catch the missile when you are hit by a ranged weapon attack. When you do so, the damage you take from the attack is reduced by 1d10 + your Dexterity modifier + your monk level.\n\nIf you reduce the damage to 0, you can catch the missile if it is small enough for you to hold in one hand and you have at least one hand free. If you catch a missile in this way, you can spend 1 ki point to make a ranged attack with the weapon or piece of ammunition you just caught, as part of the same reaction. You make this attack with proficiency, regardless of your weapon proficiencies, and the missile counts as a monk weapon for the attack, which has a normal range of 20 feet and a long range of 60 feet.'
-                },
-                'Ability Score Improvement': {
-                    source: {type: 'Class', value: 'Monk', level: 4},
-                    description: 'When you reach 4th level, and again at 8th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature.\n\nUsing the optional feats rule, you can forgo taking this feature to take a feat of your choice instead.',
-                    value: '4: DEX, DEX | 8: DEX, DEX'
-                },
-                'Slow Fall': {
-                    source: {type: 'Class', value: 'Monk', level: 4},
-                    description: 'Beginning at 4th level, you can use your reaction when you fall to reduce any falling damage you take by an amount equal to five times your monk level.'
-                },
-                'Extra Attack': {
-                    source: {type: 'Class', value: 'Monk', level: 5},
-                    description: 'Beginning at 5th level, you can attack twice, instead of once, whenever you take the Attack action on your turn.'
-                },
-                'Stunning Strike': {
-                    source: {type: 'Class', value: 'Monk', level: 5},
-                    description: 'Starting at 5th level, you can interfere with the flow of ki in an opponent’s body. When you hit another creature with a melee weapon attack, you can spend 1 ki point to attempt a stunning strike. The target must succeed on a Constitution saving throw or be stunned until the end of your next turn.'
-                },
-                'Ki-Empowered Strikes': {
-                    source: {type: 'Class', value: 'Monk', level: 6},
-                    description: 'Starting at 6th level, your unarmed strikes count as magical for the purpose of overcoming resistance and immunity to nonmagical attacks and damage.'
-                },
-                'Wholeness of Body': {
-                    source: {type: 'Class', value: 'Monk', sub: 'Way of the Open Hand', level: 6},
-                    description: 'At 6th level, you gain the ability to heal yourself. As an action, you can regain hit points equal to three times your monk level. You must finish a long rest before you can use this feature again.'
-                },
-                "Evasion": {
-                    "source": {"type": "Class", "value": "Monk", "level": 7},
-                    "description": "At 7th level, your instinctive agility lets you dodge out of the way of certain area effects, such as a blue dragon’s lightning breath or a fireball spell. When you are subjected to an effect that allows you to make a Dexterity saving throw to take only half damage, you instead take no damage if you succeed on the saving throw, and only half damage if you fail."
-                },
-                "Stillness of Mind": {
-                    "source": {"type": "Class", "value": "Monk", "level": 7},
-                    "description": "Starting at 7th level, you can use your action to end one effect on yourself that is causing you to be charmed or frightened."
-                }
-            }
+            // Derived at runtime
+            ac: 0,
+            hd: [],
+            initative: 0,
+            spellMeta: {},
+            skills: {}
         };
     },
     beforeMount() {
@@ -295,6 +154,16 @@ name: 'xoh',
                     (prof ? this.proficiency : 0)
             }
         });
+
+        let fullSpells = [];
+        this.spells.forEach((spell) => fullSpells.push(spells.find((s) => s.name === spell)));
+        this.spells = fullSpells;
+
+        this.spellMeta = {
+            castingMod: this.abilities.CHA.mod,
+            spellAttack: this.proficiency + this.abilities.CHA.mod,
+            spellDC: 8 + this.proficiency + this.abilities.CHA.mod
+        };
 
         console.log(this);
     }
