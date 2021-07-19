0a1408e76e049b212210000b93d6d2f201556d88
Tim Noel <tnoel@eitccorp.com>
2021-07-19 12:52:33 -0400

    Add character sheet cards for abilities and skills

--- src/views/xoh.vue
+++ src/views/xoh.vue
@@ -1,7 +1,37 @@
 <template>
-    <el-row :gutter="20" class="character">
+    <el-row class="character">
         <el-col :xs="24" :sm="8" :md="6" :xl="4">
-            <el-card class="meta">
+            <el-card class="column-card">
+                <div class="ability-row" v-for="(value, ability) in abilities" :key="ability">
+                    <div class="ability-label">
+                        <span class="short">{{ability}}</span>
+                        <span class="long">{{value.label}}</span>
+                    </div>
+                    <div class="ability-val">
+                        <span class="val">{{value.val}}</span>
+                        <span class="mod">{{value.mod > 0 ? '+' : ''}}{{value.mod}}</span>
+                    </div>
+                    <div class="ability-save" :class="{prof: value.prof}">
+                        <span class="prof-label">{{value.prof ? 'Proficient' : ''}} Save</span>
+                        <span class="save">
+                            {{value.save > 0 ? '+' : ''}}{{value.save}}
+                        </span>
+                    </div>
+                </div>
+            </el-card>
+        </el-col>
+        <el-col :xs="24" :sm="8" :md="6" :xl="4">
+            <el-card class="column-card">
+                <div class="skill-row" :class="{prof: value.prof}"
+                    v-for="(value, skill) in skills" :key="skill">
+                    <span>
+                        <span class="skill-ability">{{value.ability}}</span>
+                        <span class="skill-name">{{skill}}</span>
+                    </span>
+                    <span class="skill-mod">
+                        {{value.mod > 0 ? '+' : ''}}{{value.mod}}
+                    </span>
+                </div>
             </el-card>
         </el-col>
         <el-col :xs="24" :sm="8" :md="6" :xl="4">
@@ -80,12 +110,12 @@ name: 'xoh',
                 max: 8
             },
             abilities: {
-                'STR': {val: 7, prof: true},
-                'DEX': {val: 20, prof: true},
-                'CON': {val: 14, prof: false},
-                'INT': {val: 8, prof: false},
-                'WIS': {val: 14, prof: false},
-                'CHA': {val: 14, prof: false}
+                'STR': {val: 7, prof: true, label: 'Strength'},
+                'DEX': {val: 20, prof: true, label: 'Dexterity'},
+                'CON': {val: 14, prof: false, label: 'Constitution'},
+                'INT': {val: 8, prof: false, label: 'Intelligence'},
+                'WIS': {val: 14, prof: false, label: 'Wisdom'},
+                'CHA': {val: 14, prof: false, label: 'Charisma'}
             },
             skillProfs: ['Acrobatics', 'Animal Handling', 'Athletics',
                 'Insight', 'Persuasion', 'Survival'],
@@ -99,6 +129,7 @@ name: 'xoh',
                     'Smith\'s Tools, Vehicles (Land)',
                 languages: 'Common, Dwarvish, Undercommon'
             },
+            currentPersona: 'Papa Kap',
             // Derived at runtime
             ac: 0,
             hd: [],
@@ -171,4 +202,128 @@ name: 'xoh',
 </script>
 
 <style scoped lang="scss">
+.character {
+    padding: 1rem 0 0 1rem;
+
+    .column-card ::v-deep(.el-card__body) {
+        padding: 1rem 0.5rem;
+    }
+
+    .column-card {
+        border-radius: 0.6rem;
+        height: 30rem;
+        margin: 0 1rem 1rem 0;
+        width: calc(100% - 1rem);
+
+        .ability-row {
+            border: 1px solid darkgray;
+            border-radius: 0.75rem;
+            display: flex;
+            flex-direction: row;
+            height: 4.25rem;
+            margin: 0.25rem 0;
+
+            .ability-label,
+            .ability-val,
+            .ability-save {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                text-align: center;
+            }
+
+            .ability-label,
+            .ability-save {
+                border-radius: 0.6rem;
+                width: 33%;
+            }
+
+            .ability-label {
+                background-color: gray;
+                border-right: 1px solid darkgray;
+                color: white;
+                font-weight: bold;
+
+                .long {
+                    color: lightgray;
+                    font-size: 0.65rem;
+                }
+
+                .short {
+                    font-size: 1.5rem;
+                }
+            }
+
+            .ability-save {
+                border-left: 1px solid darkgray;
+                font-weight: bold;
+
+                .prof-label {
+                    color: gray;
+                    font-size: 0.75rem;
+                }
+
+                &.prof {
+                    background-color: gray;
+                    color: white;
+
+                    .prof-label {
+                        color: lightgray;
+                    }
+                }
+
+                .save {
+                    font-size: 1.5rem;
+                }
+            }
+
+            .ability-val {
+                font-weight: bold;
+                width: 34%;
+
+                .mod {
+                    color: gray;
+                    font-size: 1rem;
+                }
+
+                .val {
+                    font-size: 1.5rem;
+                }
+            }
+        }
+
+        .skill-row {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            margin: 0.2rem 0;
+            padding: 0.05rem 0.5rem;
+
+            .skill-name {
+                font-weight: bold;
+            }
+
+            .skill-ability {
+                font-size: 0.8rem;
+                font-weight: bold;
+                margin-right: 0.25rem;
+                color: darkgray;
+            }
+
+            .skill-mod {
+                margin-right: 0.25rem;
+            }
+
+            &.prof {
+                background-color: gray;
+                border-radius: 0.5rem;
+                color: white;
+
+                .skill-ability {
+                    color: lightgray;
+                }
+            }
+        }
+    }
+}
 </style>
